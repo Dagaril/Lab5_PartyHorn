3889dd942fe8fd29939855ac1f736edc3b9cc833
Dagaril <dmagaril@ucsd.edu>
2021-02-02 17:15:00 -0800

    Button works and is disabled appropriately

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,11 +8,12 @@ var play = document.getElementById("honk-btn");
 var audio = document.getElementById("horn-sound");
 var sndImg = document.getElementById("sound-image");
 
-play.addEventListener("mouseover", audio.play());
+
+
 volNum.addEventListener("input",function(){updateFromNumber(volNum.value,volSli, audio, volIma)});
 volSli.addEventListener("input", function(){updateFromSlider(volSli.value, volNum, audio, volIma)});
 aSelect.addEventListener("change",function(){setAudio(sndImg, audio)});
-
+play.addEventListener("click", function(event){event.preventDefault(); audio.play();});
 
 
 
@@ -30,7 +31,6 @@ function setAudio(image, audio){
         audio.src="./assets/media/audio/car-horn.mp3";
         image.src="./assets/media/images/car.svg";
     }
-    audio.play();
 }
 
 
@@ -47,13 +47,19 @@ function updateFromNumber(numVal, volumeSlider, audio, image){
 }
 
 function updateImage(image, sliVal){
+    var btn = document.getElementById("honk-btn");
     if(sliVal==0){
         image.src="./assets/media/icons/volume-level-0.svg";
+        btn.disabled = true;
+        return;
     } else if (sliVal <= 33){
         image.src="./assets/media/icons/volume-level-1.svg";
     } else if (sliVal <= 66){
         image.src="./assets/media/icons/volume-level-2.svg";
     } else{
         image.src="./assets/media/icons/volume-level-3.svg";
+    }
+    if(btn.disabled){
+        btn.disabled=false;
     }
 }
